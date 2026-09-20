12ef7469acb903e2ad96bbd194bf8320d455a847
Ali <thealisife14@gmail.com>
2026-09-20 08:22:29 +0200

    fix(cors): add ui-rehaul preview branch alias to ALLOWED_ORIGINS, it allows the newly automated cloudflare pages preview deployements on the ui/rehaul branch on the react rebuild to succesfully fetch card data from the proxy worker

--- tcg-proxy/src/index.js
+++ tcg-proxy/src/index.js
@@ -6,6 +6,7 @@
 const ALLOWED_ORIGINS = [
   "https://pokemon-finder-5wx.pages.dev",
   "http://localhost:5173",
+  "https://ui-rehaul.pokemon-finder-react-b8o.pages.dev",
 ];
 
 //the decision function
